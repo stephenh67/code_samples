a2d23cb058d6eb9bb833ef017b21994669e7cdc4
stephenh67 <sbhope67@gmail.com>
2020-08-17 08:35:36 -0400

    add

---
+++ hamgman.py
@@ -0,0 +1,54 @@
+import sys
+
+
+def play():
+    words = ['python', 'java', 'kotlin', 'javascript']
+    word = random.choice(words)
+
+    hidden_word = '-' * len(word)
+    tries = 0
+    used_letters = []
+
+    while tries < 8:
+
+        print(hidden_word)
+        guess = input("Input a letter:")
+
+        if guess in used_letters and not len(guess) > 1:
+            print("You already typed this letter")
+        elif len(guess) > 1:
+            print("You should input a single letter")
+        elif not guess.isalpha() or not guess.islower():
+            print("It is not an ASCII lowercase letter")
+        elif guess not in word:
+            print("No such letter in the word")
+            tries += 1
+        else:
+            hidden_word_lst = list(hidden_word)
+            word_lst = list(word)
+            for index in range(len(word)):
+                if word[index] == guess:
+                    hidden_word_lst[index] = guess
+            hidden_word = ''.join(hidden_word_lst)
+
+        used_letters.append(guess)
+
+        if word == hidden_word:
+            print(f"You guessed the word {word}!")
+            print("You survived!")
+            break
+
+        if tries > 7 and word != hidden_word:
+            print('You are hanged!')
+            break
+        print()
+
+
+while True:
+    print('H A N G M A N')
+    play_now = input('Type "play" to play the game, "exit" to quit: ')
+    print()
+    if play_now == 'play':
+        play()
+    elif play_now == 'exit':
+        sys.exit()
